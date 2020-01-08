82fb17fff5a7d5eec4790d6f3ce04bd8c5a3e3ec
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-07 22:24:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    const config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    }
+    return fetch("http://localhost:3000/users", config)
+        .then(resp => resp.json())
+        .then(function(object) {
+            const p = document.createElement('p')
+            p.textContent = object.id
+            document.body.appendChild(p)
+        }).catch(function(error) {
+            const p = document.createElement('p')
+            p.textContent = error.message
+            document.body.appendChild(p)
+        })
+}
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    submitData('hello', 'there')
+})
